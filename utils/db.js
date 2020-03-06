c04f4def3e0af133675e20f77ab5b9b259849b7e
todoroff <stoyansky@gmail.com>
2020-03-06 11:25:01 +0000

    db connection

---
+++ utils/db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const db = process.env.MONGO_URI;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    });
+    console.log("MongoDB Connected....");
+  } catch (err) {
+    console.log(err.message);
+    //Exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
