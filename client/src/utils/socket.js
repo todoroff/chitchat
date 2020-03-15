8033641223f95b925b45aa6bb5361b244bd0d3a3
todoroff <stoyansky@gmail.com>
2020-03-15 19:36:27 +0000

    logout on autherror

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,17 +1,22 @@
 import io from "socket.io-client";
+import store from "../store";
 
 const socket = io("https://localhost:8443");
 
 export function send(route, method, payload) {
   return new Promise((resolve, reject) => {
-    function onResponse(response = {}) {
+    async function onResponse(response = {}) {
       if (response.error) {
+        console.log(response);
+        //if missing logged in session, set loggedIn state to false
+        if (response.error.type === "authorizationError") {
+          await store.dispatch({ type: "AUTH_FAILURE" });
+        }
         reject(response.error);
       } else {
         resolve(response);
       }
     }
-    console.log('sent')
     socket.emit(route, { method, payload }, onResponse);
   });
 }
